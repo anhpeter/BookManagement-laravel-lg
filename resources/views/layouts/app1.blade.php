de5258d79bb67a2014eda14bde8805841ae2367c
Peter Anh <webfullstack99@gmail.com>
2021-07-27 16:58:02 +0700

    add admin theme

---
+++ resources/views/layouts/app1.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('includes/head')
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        @include('includes/sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                @include('includes/nav')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+
+            </div>
+
+            <!-- Footer -->
+            @include('includes/main-footer')
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+    @include('includes/footer')
+
+
+    <!-- SCRIPTS -->
+    @include('includes/scripts')
+</body>
+
+</html>
