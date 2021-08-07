3acf8fea918c6fc0e715bb26252ec3d64ab99d41
Peter Anh <webfullstack99@gmail.com>
2021-08-07 12:21:02 +0700

    add article and tag table, fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use App\Models\Book;
 use App\Models\Category;
 use App\Models\Group;
 use App\Models\Profile;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
 
@@ -21,7 +23,14 @@ class DatabaseSeeder extends Seeder
         Group::factory(3)->create();
         User::factory(10)->create();
         Profile::factory(1)->create();
-        Category::factory(5)->create();
-        Book::factory(30)->create();
+        //Category::factory(5)->create();
+        //Book::factory(30)->create();
+         Tag::factory(20)->create();
+         Article::factory(50)->create()->each(function($article){
+            $ids = range(1, 20);
+            shuffle($ids);//trộn
+            $sliced = array_slice($ids, 1, 10);
+            //$article->tags()->attach($sliced);
+         });
     }
 }
